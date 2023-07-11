c9baeda7d81438b7ff4634030a541ccc0e4bab66
Gilberto Olivas <jose.olivas@bigcommerce.com>
2023-07-11 15:54:07 -0700

    create custom macros

--- config/macros/global.h
+++ config/macros/global.h
@@ -6,10 +6,15 @@
                   label = "pwdmanager";
                   compatible = "zmk,behavior-macro";
                   #binding-cells = <0>;
+                  tap-ms = <1>;
+                  wait-ms = <1>;
                   bindings
-                    = <&kp M &kp y &kp N &kp a &kp m &kp e &kp I &kp s &kp G &kp i &kp l &kp b &kp e &kp r &kp t &kp o \
-                    &kp A &kp n &kp d &kp M &kp y &kp G &kp i &kp r &kp l &kp f &kp r &kp i &kp e &kp n &kp d &kp I \
-                    &kp s &kp M &kp e &kp l &kp i &kp s &kp s &kp a>;
+                    = <&kp M &kp y &kp N &kp a &kp m &kp e &kp I &kp s>
+                    , <&kp G &kp i &kp l &kp b &kp e &kp r &kp t &kp o>
+                    , <&kp A &kp n &kp d &kp M &kp y &kp G &kp i &kp r>
+                    , <&kp l &kp f &kp r &kp i &kp e &kp n &kp d &kp I>
+                    , <&kp s &kp M &kp e &kp l &kp i &kp s &kp s &kp a>
+                    ;
                 };
   };
 };
