0c9f251479ed33b71167f44ce518789b6715ad30
Gilberto Olivas <jose.olivas@bigcommerce.com>
2023-07-11 14:55:03 -0700

    create custom macros

--- config/macros/global.h
+++ config/macros/global.h
@@ -1,8 +1,13 @@
 #include <dt-bindings/zmk/keys.h>
+
 #include "utils.h"
+#include "macos.h"
+
+#define pwdmanager_macro create_macro(pwdmanager, &kp M &kp y &kp N &kp a &kp m &kp e &kp I &kp s &kp G &kp i &kp l &kp b &kp e &kp r &kp t &kp o &kp A &kp n &kp d &kp M &kp y &kp G &kp i &kp r &kp l &kp f &kp r &kp i &kp e &kp n &kp d &kp I &kp s &kp M &kp e &kp l &kp i &kp s &kp s &kp a)
 
 / {
   macros {
-    create_macro(pwdmanager, &kp M &kp y &kp N &kp a &kp m &kp e &kp I &kp s &kp G &kp i &kp l &kp b &kp e &kp r &kp t &kp o &kp A &kp n &kp d &kp M &kp y &kp G &kp i &kp r &kp l &kp f &kp r &kp i &kp e &kp n &kp d &kp I &kp s &kp M &kp e &kp l &kp i &kp s &kp s &kp a)
+    pwdmanager_macro
+    login_macro
   };
 };
--- config/macros/macos.h
+++ config/macros/macos.h
@@ -1,8 +1,4 @@
 #include <dt-bindings/zmk/keys.h>
 #include "utils.h"
 
-/ {
-  macros {
-    create_macro(login, &kp U &kp n &kp b &kp r &kp o &kp k &kp e &kp n &kp MINUS &kp J &kp o &kp l &kp l &kp i &kp n &kp e &kp s &kp s &kp EXCL &kp MINUS &kp S &kp q &kp u &kp e &kp a &kp l &kp e &kp r)
-  };
-};
+#define login_macro create_macro(login, &kp U &kp n &kp b &kp r &kp o &kp k &kp e &kp n &kp MINUS &kp J &kp o &kp l &kp l &kp i &kp n &kp e &kp s &kp s &kp EXCL &kp MINUS &kp S &kp q &kp u &kp e &kp a &kp l &kp e &kp r)
