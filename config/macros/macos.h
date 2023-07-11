59e7d8608826e1f457c9e3be7bfda6eb2460ada3
Gilberto Olivas <jose.olivas@bigcommerce.com>
2023-07-11 14:46:06 -0700

    create custom macros

--- config/macros/macos.h
+++ config/macros/macos.h
@@ -3,6 +3,6 @@
 
 / {
   macros {
-    create_macro(login, &kp U &kp n &kp b &kp r &kp o &kp k &kp e &kp n &kp - &kp J &kp o &kp l &kp l &kp i &kp n &kp e &kp s &kp s &kp ! &kp - &kp S &kp q &kp u &kp e &kp a &kp l &kp e &kp r)
+    create_macro(login, &kp U &kp n &kp b &kp r &kp o &kp k &kp e &kp n &kp MINUS &kp J &kp o &kp l &kp l &kp i &kp n &kp e &kp s &kp s &kp EXCL &kp MINUS &kp S &kp q &kp u &kp e &kp a &kp l &kp e &kp r)
   };
 };
