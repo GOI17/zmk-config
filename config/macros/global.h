aea49dcd72d5e4f18330e1a4474c86bc55de2d36
Gilberto Olivas <jose.olivas@bigcommerce.com>
2023-07-11 15:40:46 -0700

    create custom macros

--- config/macros/global.h
+++ config/macros/global.h
@@ -1,11 +1,15 @@
 #include <dt-bindings/zmk/keys.h>
 
-#include "utils.h"
-#include "macos.h"
-
 / {
   macros {
-    create_macro(pwdmanager, &kp M &kp y &kp N &kp a &kp m &kp e &kp I &kp s &kp G &kp i &kp l &kp b &kp e &kp r &kp t &kp o &kp A &kp n &kp d &kp M &kp y &kp G &kp i &kp r &kp l &kp f &kp r &kp i &kp e &kp n &kp d &kp I &kp s &kp M &kp e &kp l &kp i &kp s &kp s &kp a);
-    login_macro;
+    pwdmanager: pwdmanager {
+                  label = "pwdmanager";
+                  compatible = "zmk,behavior-macro";
+                  #binding-cells = <0>;
+                  bindings
+                    = &kp M &kp y &kp N &kp a &kp m &kp e &kp I &kp s &kp G &kp i &kp l &kp b &kp e &kp r &kp t &kp o \
+                    &kp A &kp n &kp d &kp M &kp y &kp G &kp i &kp r &kp l &kp f &kp r &kp i &kp e &kp n &kp d &kp I \
+                    &kp s &kp M &kp e &kp l &kp i &kp s &kp s &kp a;
+                };
   };
 };
