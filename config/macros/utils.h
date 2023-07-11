2f638d4dacaeff96b110e29bcb477e0cbc1916fc
Gilberto Olivas <jose.olivas@bigcommerce.com>
2023-07-11 14:18:47 -0700

    create custom macros

--- config/macros/utils.h
+++ config/macros/utils.h
@@ -1,9 +1,10 @@
-#define create_macro(name, keys)
-name: name##_macro {
-        label = #name;
-        compatible = "zmk,behavior-macro";
-        #binding-cells = <0>;
-        tap-ms = <1>;
-        wait-ms = <1>;
-        bindings = <keys>;
-      };
+#define create_macro(name, keys)    \
+name: name##_macro {                \
+	label = #name;                    \
+	compatible = "zmk,behavior-macro";\
+	#binding-cells = <0>;             \
+	tap-ms = <1>;                     \
+	wait-ms = <1>;                    \
+	bindings = <keys>;                \
+};
+
