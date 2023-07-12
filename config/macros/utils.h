51f62f261be0bbf8358bb5f4bd17ebd686ea9518
Gilberto Olivas <jose.olivas@bigcommerce.com>
2023-07-11 21:29:01 -0700

    create custom macros

--- config/macros/utils.h
+++ config/macros/utils.h
@@ -1,1 +1,8 @@
-#define create_macro(name, keys) ZMK_MACRO(name, label = name; tap-ms = <1>; wait-ms = <1>; bindings = <keys>; )
+void create_macro(name, keys) {
+  ZMK_MACRO(name,
+      label = name;
+      tap-ms = <1>;
+      wait-ms = <1>;
+      bindings = <keys>;
+  )
+}
