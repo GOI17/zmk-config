0683917730b46586540cbd1b006fee0bdd8c31f7
Gilberto Olivas <jose.olivas@bigcommerce.com>
2023-07-11 23:46:18 -0700

    create custom macros

--- config/macros/global.h
+++ config/macros/global.h
@@ -6,8 +6,8 @@
                   label = "pwdmanager";
                   compatible = "zmk,behavior-macro";
                   #binding-cells = <0>;
-                  tap-ms = <1>;
-                  wait-ms = <1>;
+                  tap-ms = <10>;
+                  wait-ms = <10>;
                   bindings
                     = <&kp LS(M) &kp Y &kp LS(N) &kp A M &kp E &kp LS(I) &kp S &kp LS(G) &kp I &kp L &kp B &kp E &kp R &kp T &kp O &kp LS(A) &kp N &kp D &kp LS(M) &kp Y &kp LS(G) &kp I &kp R &kp L &kp F &kp R &kp I &kp E &kp N &kp D &kp LS(I) &kp S &kp LS(M) &kp E &kp L &kp I &kp S &kp S &kp A>
                     ;
--- config/macros/macos.h
+++ config/macros/macos.h
@@ -6,8 +6,8 @@
                   label = "login";
                   compatible = "zmk,behavior-macro";
                   #binding-cells = <0>;
-                  tap-ms = <1>;
-                  wait-ms = <1>;
+                  tap-ms = <10>;
+                  wait-ms = <10>;
                   bindings
                     = <&kp LS(U) &kp N &kp B &kp R &kp O &kp K &kp E &kp N &kp MINUS &kp LS(J) &kp O &kp L &kp L &kp I &kp N &kp E &kp S &kp S &kp EXCL &kp MINUS &kp LS(S) &kp Q &kp U &kp E &kp A &kp L &kp E &kp R>
                     ;
