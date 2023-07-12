1c134fb5e5630c5f13965273eb5e37087ca97d19
Gilberto Olivas <jose.olivas@bigcommerce.com>
2023-07-11 21:50:28 -0700

    create custom macros

--- config/macros/global.h
+++ config/macros/global.h
@@ -1,10 +1,16 @@
 #include <dt-bindings/zmk/keys.h>
-#include <behaviors.dtsi>
-
-#include "utils.h"
 
 / {
   macros {
-    create_macro(pwd_manager, &kp LS(M) &kp Y &kp LS(N) &kp A M &kp E &kp LS(I) &kp S &kp LS(G) &kp I &kp L &kp B &kp E &kp R &kp T &kp O &kp LS(A) &kp N &kp D &kp LS(M) &kp Y &kp LS(G) &kp I &kp R &kp L &kp F &kp R &kp I &kp E &kp N &kp D &kp LS(I) &kp S &kp LS(M) &kp E &kp L &kp I &kp S &kp S &kp A)
+    pwdmanager: pwdmanager {
+                  label = "pwdmanager";
+                  compatible = "zmk,behavior-macro";
+                  #binding-cells = <0>;
+                  tap-ms = <1>;
+                  wait-ms = <1>;
+                  bindings
+                    = <&kp LS(M) &kp Y &kp LS(N) &kp A M &kp E &kp LS(I) &kp S &kp LS(G) &kp I &kp L &kp B &kp E &kp R &kp T &kp O &kp LS(A) &kp N &kp D &kp LS(M) &kp Y &kp LS(G) &kp I &kp R &kp L &kp F &kp R &kp I &kp E &kp N &kp D &kp LS(I) &kp S &kp LS(M) &kp E &kp L &kp I &kp S &kp S &kp A>
+                    ;
+                };
   };
 };
--- config/macros/macos.h
+++ config/macros/macos.h
@@ -1,10 +1,16 @@
 #include <dt-bindings/zmk/keys.h>
-#include <behaviors.dtsi>
-
-#include "utils.h"
 
 / {
   macros {
-    create_macro(login, &kp LS(U) &kp N &kp B &kp R &kp O &kp K &kp E &kp N &kp MINUS &kp LS(J) &kp O &kp L &kp L &kp I &kp N &kp E &kp S &kp S &kp EXCL &kp MINUS &kp LS(S) &kp Q &kp U &kp E &kp A &kp L &kp E &kp R)
+    login: login {
+                  label = "login";
+                  compatible = "zmk,behavior-macro";
+                  #binding-cells = <0>;
+                  tap-ms = <1>;
+                  wait-ms = <1>;
+                  bindings
+                    = <&kp LS(U) &kp N &kp B &kp R &kp O &kp K &kp E &kp N &kp MINUS &kp LS(J) &kp O &kp L &kp L &kp I &kp N &kp E &kp S &kp S &kp EXCL &kp MINUS &kp LS(S) &kp Q &kp U &kp E &kp A &kp L &kp E &kp R>
+                    ;
+                };
   };
 };
