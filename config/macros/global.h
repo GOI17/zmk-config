8f601a166376f0e9a04b38bace59541d4911848f
Gilberto Olivas <jose.olivas@bigcommerce.com>
2023-07-11 21:05:17 -0700

    create custom macros

--- config/macros/global.h
+++ config/macros/global.h
@@ -5,6 +5,6 @@
 
 / {
   macros {
-    create_macro(pwdmanager, &kp LS(M) &kp Y &kp LS(N) &kp A M &kp E &kp LS(I) &kp S &kp LS(G) &kp I &kp L &kp B &kp E &kp R &kp T &kp O &kp LS(A) &kp N &kp D &kp LS(M) &kp Y &kp LS(G) &kp I &kp R &kp L &kp F &kp R &kp I &kp E &kp N &kp D &kp LS(I) &kp S &kp LS(M) &kp E &kp L &kp I &kp S &kp S &kp A)
+    create_macro(pwd_manager, &kp LS(M) &kp Y &kp LS(N) &kp A M &kp E &kp LS(I) &kp S &kp LS(G) &kp I &kp L &kp B &kp E &kp R &kp T &kp O &kp LS(A) &kp N &kp D &kp LS(M) &kp Y &kp LS(G) &kp I &kp R &kp L &kp F &kp R &kp I &kp E &kp N &kp D &kp LS(I) &kp S &kp LS(M) &kp E &kp L &kp I &kp S &kp S &kp A)
   };
 };
--- config/macros/utils.h
+++ config/macros/utils.h
@@ -1,10 +1,1 @@
-#define create_macro(name, keys)    \
-name: #name {                       \
-	label = ZM_##name;                    \
-	compatible = "zmk,behavior-macro";\
-	#binding-cells = <0>;             \
-	tap-ms = <1>;                     \
-	wait-ms = <1>;                    \
-	bindings = <keys>;                \
-};
-
+#define create_macro(name, keys) ZMK_MACRO(name, tap-ms = <1>; wait-ms = <1>; bindings = <keys>; )
