b0b706790042cda13c92c3ddd6decad76852cbaf
Gilberto Olivas <jose.olivas@bigcommerce.com>
2023-07-24 13:45:37 -0700

    create custom macros

--- config/macros/all.h
+++ config/macros/all.h
@@ -1,5 +1,5 @@
 / {
   macros {
-    create_macro("pwdmanager", &kp LS(M) &kp Y &kp LS(N) &kp A M &kp E &kp LS(I) &kp S &kp LS(G) &kp I);
-    create_macro("login", &kp LS(U) &kp N &kp B &kp R &kp O &kp K &kp E &kp N &kp MINUS);
+    create_macro("pwdmanager", &kp LS(M) &kp Y &kp LS(N) &kp A M &kp E &kp LS(I) &kp S &kp LS(G) &kp I)
+    create_macro("login", &kp LS(U) &kp N &kp B &kp R &kp O &kp K &kp E &kp N &kp MINUS)
 };
