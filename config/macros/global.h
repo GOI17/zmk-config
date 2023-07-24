e1731daa2eb90a324bf6c30c3e9f3233e8d13b2b
Gilberto Olivas <jose.olivas@bigcommerce.com>
2023-07-24 11:30:15 -0700

    create custom macros

--- config/macros/global.h
+++ config/macros/global.h
@@ -3,5 +3,4 @@
 #include "utils.h"
 
 // #define keys &kp LS(M) &kp Y &kp LS(N) &kp A M &kp E &kp LS(I) &kp S &kp LS(G) &kp I &kp L &kp B &kp E &kp R &kp T &kp O &kp LS(A) &kp N &kp D &kp LS(M) &kp Y &kp LS(G) &kp I &kp R &kp L &kp F &kp R &kp I &kp E &kp N &kp D &kp LS(I) &kp S &kp LS(M) &kp E &kp L &kp I &kp S &kp S &kp A;
-#define keys &kp LS(M) &kp Y &kp LS(N) &kp A M &kp E &kp LS(I) &kp S &kp LS(G) &kp I;
-#define pwdmanager_macro create_macro("pwdmanager", keys);
+#define pwdmanager_macro create_macro("pwdmanager", &kp LS(M) &kp Y &kp LS(N) &kp A M &kp E &kp LS(I) &kp S &kp LS(G) &kp I);
--- config/macros/macos.h
+++ config/macros/macos.h
@@ -3,5 +3,4 @@
 #include "utils.h"
 
 // #define keys &kp LS(U) &kp N &kp B &kp R &kp O &kp K &kp E &kp N &kp MINUS &kp LS(J) &kp O &kp L &kp L &kp I &kp N &kp E &kp S &kp S &kp EXCL &kp MINUS &kp LS(S) &kp Q &kp U &kp E &kp A &kp L &kp E &kp R;
-#define keys &kp LS(U) &kp N &kp B &kp R &kp O &kp K &kp E &kp N &kp MINUS;
-#define login_macro create_macro("login", keys);
+#define login_macro create_macro("login", &kp LS(U) &kp N &kp B &kp R &kp O &kp K &kp E &kp N &kp MINUS);
