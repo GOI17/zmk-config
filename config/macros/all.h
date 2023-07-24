db628d921a38af833495df639063b7387120bdf6
Gilberto Olivas <jose.olivas@bigcommerce.com>
2023-07-24 14:56:10 -0700

    create custom macros

--- config/macros/all.h
+++ config/macros/all.h
@@ -1,10 +1,8 @@
 #include <dt-bindings/zmk/keys.h>
 
-#include "utils.h"
-
 / {
   macros {
-    create_macro(pwdmanager, &kp LS(M) &kp Y &kp LS(N) &kp A M &kp E &kp LS(I) &kp S &kp LS(G) &kp I)
-    create_macro(login, &kp LS(U) &kp N &kp B &kp R &kp O &kp K &kp E &kp N &kp MINUS)
+    ZMK_MACRO(pwdmanager, bindings = <&kp LS(M) &kp Y &kp LS(N) &kp A M &kp E &kp LS(I) &kp S &kp LS(G) &kp I>; tap-ms = <1>; wait-ms = <1>;)
+    ZMK_MACRO(login, bindings = <&kp LS(U) &kp N &kp B &kp R &kp O &kp K &kp E &kp N &kp MINUS>; tap-ms = <1>; wait-ms = <1>;)
   };
 };
